61d32be38b29e7a93f4b11ef6581c6bab8220a07
Jose Jimenez <jjimenez@eaglepassisd.net>
2023-05-23 21:40:22 -0500

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,16 @@ import { getFirestore}  from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCt1C6mVN7QiV0xdYNWAiZO-tJzlxqS_Tg",
   authDomain: "crypto-galaxy-364614.firebaseapp.com",
   projectId: "crypto-galaxy-364614",
   storageBucket: "crypto-galaxy-364614.appspot.com",
-  messagingSenderId: "130822631899",
-  appId: "1:130822631899:web:c37f9d92519db075ccb7fe"
+  messagingSenderId:"130822631899",
+  appId: "130822631899:web:c37f9d92519db075ccb7fe"
 };
 
 // Initialize Firebase
